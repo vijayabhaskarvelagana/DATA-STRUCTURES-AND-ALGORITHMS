306c600540014c4fb025e1551b4db8a8f632ee42
Vijaya Bhaskar Velagana <138884830+vijayabhaskarvelagana@users.noreply.github.com>
2026-04-29 10:09:19 +0530

    Create bubble-sort.py

---
+++ bubble-sort.py
@@ -0,0 +1,17 @@
+arr = [1, -1, 0, 2, -4, 100, -100]
+
+# bubble sort algorithm
+# swap consecutive elements n-1 times 
+# for each loop highest element is placed at its correct position
+
+count = 1
+n = len(arr)
+while count <= n-1:
+    i = 0
+    while i <= n-1-count:
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+        i += 1
+    count += 1
+    
+print(arr)
