7eae4078e8d08d56e096408faaf2a61430af85de
Vijaya Bhaskar Velagana <138884830+vijayabhaskarvelagana@users.noreply.github.com>
2026-06-30 13:48:52 +0530

    Update get-factors.py

--- get-factors.py
+++ get-factors.py
@@ -13,3 +13,25 @@ print(f"res: {res}")
 
 # TC: O(sqrt(n))
 # SC: O(f) ; f = number of factors
+
+
+# ======================
+
+
+
+num = int(input("Enter the value of num: "))
+
+sqrt_num = int(num ** 0.5)
+
+res = list()
+for i in range(1, sqrt_num+1):
+    if num % i == 0:
+        res.append(i)
+        if int(num/i) != i:
+            res.append(int(num/i))
+            
+print(f"res: {res}")
+
+# TC: O(sqrt(N))
+# SC: O(F)
+
